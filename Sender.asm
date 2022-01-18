39a0108a5a8adaa5e0ed3092f49699cb5f877e4b
karimkohel <kareemkohel@gmail.com>
2022-01-18 16:10:26 +0200

    FINISHED PROJECT

--- Sender.asm
+++ Sender.asm
@@ -13,20 +13,18 @@ E  BIT P3.5
 
 
 ;LCD INITIALIZATION
-	MOV A, #38H	; INITIATE LCD ; 00 0011 1000 ; sets interface length , number of lines, and font
-	ACALL COMMANDWRT
-	ACALL DELAY
+MOV A, #38H	; INITIATE LCD ; 00 0011 1000 ; sets interface length , number of lines, and font
+ACALL COMMANDWRT
+ACALL DELAY
 
-	MOV A, #0CH	; DISPLAY & CURSOR ON ; 00 0000 1100
-	ACALL COMMANDWRT
-	ACALL DELAY
+MOV A, #0CH	; DISPLAY & CURSOR ON ; 00 0000 1100
+ACALL COMMANDWRT
+ACALL DELAY
 		
-	ACALL CLRLCD
+ACALL CLRLCD    ; clear lcd to prepare for other strings
 	
 	
 ; initializing and reading keypad continuously
-;MOV DPTR,#DIGITS ;// moves starting address of DIGITS to DPTR
-
 BACK:
      MOV P1,#11111111B ;// loads P1 with all 1's
      CLR P1.0  ;// makes row 1 low
@@ -54,60 +52,99 @@ NEXT3:
       JB P1.7,NEXT4
       MOV A,#'A'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #SAPA
+      ACALL STRING
 NEXT4:
       SETB P1.0
       CLR P1.1
       JB P1.4,NEXT5
       MOV A,#'4'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #AP4
+      ACALL STRING
 NEXT5:
       JB P1.5,NEXT6
       MOV A,#'5'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #AP5
+      ACALL STRING
 NEXT6:
       JB P1.6,NEXT7
       MOV A,#'6'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #AP6
+      ACALL STRING
 NEXT7:
       JB P1.7,NEXT8
       MOV A,#'B'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #SAPB
+      ACALL STRING
 NEXT8:
       SETB P1.1
       CLR P1.2
       JB P1.4,NEXT9
       MOV A,#'7'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #AP7
+      ACALL STRING
 NEXT9:
       JB P1.5,NEXT10
       MOV A,#'8'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #AP8
+      ACALL STRING
 NEXT10:
       JB P1.6,NEXT11
       MOV A,#'9'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #AP9
+      ACALL STRING
 NEXT11:
       JB P1.7,NEXT12
       MOV A,#'C'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #SAPC
+      ACALL STRING
 NEXT12:
       SETB P1.2
       CLR P1.3
       JB P1.4,NEXT13
       MOV A,#'*'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #SAPS
+      ACALL STRING
 NEXT13:
       JB P1.5,NEXT14
       MOV A,#'0'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #ZERO
+      ACALL STRING
 NEXT14:
       JB P1.6,NEXT15
       MOV A,#'#'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #SAPT
+      ACALL STRING
 NEXT15:
       JB P1.7,DONEKEYPAD
       MOV A,#'D'
       ACALL SEND
+      ACALL CLRLCD
+      MOV DPTR, #SAPD
+      ACALL STRING
       LJMP BACK
 
 DONEKEYPAD:
@@ -169,8 +206,21 @@ DATAWRT:
 	RET
 
 ORG 300H
+ZERO:    DB	"ZERO", 0
 LED:	 DB	"LED" ,0 
 FAN:	 DB	"FAN" ,0
 BUZZER:  DB     "BUZZER", 0
+AP4:     DB	"AP4", 0
+AP5:     DB	"AP5", 0
+AP6:     DB	"AP6", 0
+AP7:     DB	"AP7", 0
+AP8:     DB	"AP8", 0
+AP9:     DB	"AP9", 0
+SAPA:    DB	"SAPA", 0
+SAPB:    DB	"SAPB", 0
+SAPC:    DB	"SAPC", 0
+SAPD:    DB	"SAPD", 0
+SAPS:    DB	"SAPS", 0
+SAPT:	 DB	"SAPH", 0
 
 END
